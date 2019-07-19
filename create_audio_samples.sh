e722daa736b288fc77b0b32d7e227e215d5be995
Mercandj <modjow.jm9@gmail.com>
2019-07-19 15:25:53 +0200

    [MP3] Control MP3 convert with explicit params
    Thanks to @AudioScientist and this issue: https://github.com/Mercandj/audio-samples/issues/1
    explicit the parameters we are using to convert flac to mp3.

--- create_audio_samples.sh
+++ create_audio_samples.sh
@@ -25,8 +25,8 @@ pushd "$BASEDIR"
 
         ffmpeg -i ./sample_bee_moved_96_khz_24_bit.flac ./build/sample_bee_moved_96_khz_24_bit.wav
 
-        # https://stackoverflow.com/questions/26109837/convert-flac-to-mp3-with-ffmpeg-keeping-all-metadata
-        ffmpeg -i ./sample_bee_moved_96_khz_24_bit.flac -ab 320k -map_metadata 0 -id3v2_version 3 ./build/sample_bee_moved_48_khz.mp3
+        # https://github.com/Mercandj/audio-samples/issues/1
+        ffmpeg -i ./sample_bee_moved_96_khz_24_bit.flac -c:a libmp3lame -ar 44100 -ab 320k -map_metadata 0 -id3v2_version 3 ./build/sample_bee_moved_48_khz.mp3
 
         ffmpeg -i ./build/sample_bee_moved_96_khz_24_bit.wav -codec:a aac ./build/sample_bee_moved_aac.m4a
 
